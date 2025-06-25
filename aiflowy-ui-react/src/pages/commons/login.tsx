8a4ab294591f9f68fcc2bf18ecbf5b92f4d196a6
18586361686 <1207669650@qq.com>
2025-06-25 17:52:08 +0800

    build: v1.0.8 release (^.^)YYa!!

--- aiflowy-ui-react/src/pages/commons/login.tsx
+++ aiflowy-ui-react/src/pages/commons/login.tsx
@@ -165,7 +165,7 @@ const Login: React.FC = () => {
                 <a href="https://aiflowy.tech " style={{ color: "#fff" }} target="_blank">
                     AIFlowy.tech
                 </a>{" "}
-                (v1.0.7)
+                (v1.0.8)
             </div>
         </div>
     ) : (
